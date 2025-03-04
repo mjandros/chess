9f7aa5eed14e2c659dab3d1e33387e2df761de25
mjandros <163360746+mjandros@users.noreply.github.com>
2025-03-03 17:10:36 -0700

    Implement UserDAO interface and MemoryUserDAO class

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,5 +1,22 @@
 package dataaccess;
 
+import java.util.HashMap;
+import model.UserData;
 
 public class MemoryUserDAO implements UserDAO {
+    final private HashMap<String, UserData> users = new HashMap<>();
+
+    public void createUser(UserData userData) throws DataAccessException {
+        if (users.containsKey(userData.username())) {
+            throw new DataAccessException("User already exists");
+        }
+        users.put(userData.username(), userData);
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        if (!users.containsKey(username)) {
+            throw new DataAccessException("User does not exist");
+        }
+        return users.get(username);
+    }
 }
