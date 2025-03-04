31b69cd9316af3cf886a8a7e34d72b7034458fab
mjandros <163360746+mjandros@users.noreply.github.com>
2025-03-03 17:20:04 -0700

    Implement AuthDAO interface and MemoryAuthDAO class

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -4,4 +4,8 @@ import model.AuthData;
 
 public interface AuthDAO {
 
+    void createAuth(AuthData authData) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+    void clear();
 }
--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,5 +1,34 @@
 package dataaccess;
 
+import model.AuthData;
+
+import java.util.HashMap;
 
 public class MemoryAuthDAO implements AuthDAO {
+    final private HashMap<String, AuthData> auths = new HashMap<>();
+
+    public void createAuth(AuthData authData) throws DataAccessException {
+        if (auths.containsKey(authData.authToken())) {
+            throw new DataAccessException("Token already exists");
+        }
+        auths.put(authData.authToken(), authData);
+    }
+
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        if (!auths.containsKey(authToken)) {
+            throw new DataAccessException("Token does not exist");
+        }
+        return auths.get(authToken);
+    }
+
+    public void deleteAuth(String authToken) throws DataAccessException {
+        if (!auths.containsKey(authToken)) {
+            throw new DataAccessException("Token does not exist");
+        }
+        auths.remove(authToken);
+    }
+
+    public void clear() {
+        auths.clear();
+    }
 }
