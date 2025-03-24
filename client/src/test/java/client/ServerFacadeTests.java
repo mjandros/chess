8931d394f85a79ea4ee0addb653f57aadac42014
mjandros <163360746+mjandros@users.noreply.github.com>
2025-03-23 22:54:54 -0600

    Add and pass register tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,22 @@
 package client;
 
+import exception.ResponseException;
 import org.junit.jupiter.api.*;
 import server.Server;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -20,10 +24,18 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void clearDB() throws ResponseException { facade.clear(); }
+
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void registerPos() throws Exception {
+        var authData = facade.register("player1", "password", "p1@email.com");
+        assertTrue(authData.authToken().length() > 10);
+    }
+    @Test
+    void registerNeg() {
+        assertThrows(ResponseException.class, () -> facade.register(null, null, null));
     }
 
 }
