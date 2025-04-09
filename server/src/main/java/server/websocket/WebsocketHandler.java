c53381fcdf1445083ca11f0119471cfe7b19d8e1
mjandros <163360746+mjandros@users.noreply.github.com>
2025-04-09 14:00:29 -0600

    Implement WebsocketHandler.connect

--- server/src/main/java/server/websocket/WebsocketHandler.java
+++ server/src/main/java/server/websocket/WebsocketHandler.java
@@ -1,6 +1,9 @@
 package server.websocket;
 
 import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import exception.ResponseException;
 import model.GameData;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
@@ -8,6 +11,7 @@ import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import server.Server;
 import websocket.commands.UserGameCommand;
 import chess.ChessMove;
+import websocket.commands.types.MakeMoveCommand;
 import websocket.messages.ServerMessage;
 import websocket.messages.types.LoadGameMessage;
 import websocket.messages.types.NotificationMessage;
@@ -17,13 +21,24 @@ import java.io.IOException;
 @WebSocket
 public class WebsocketHandler {
     private final ConnectionManager connections = new ConnectionManager();
+    private final GameDAO gameDAO;
+
+    public WebsocketHandler(GameDAO gameDAO) {
+        this.gameDAO = gameDAO;
+    }
 
     @OnWebSocketMessage
-    public void onMessage(Session session, String message) throws IOException {
-        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+    public void onMessage(Session session, String message) throws IOException, ResponseException, DataAccessException {
+        UserGameCommand command;
+        if (message.contains("move")) {
+            command = new Gson().fromJson(message, MakeMoveCommand.class);
+        } else {
+            command = new Gson().fromJson(message, UserGameCommand.class);
+        }
+        GameData game = gameDAO.getGame(command.getGameID());
         switch (command.getCommandType()) {
-            //case CONNECT -> connect(command.getUsername(), session);
-            //case MAKE_MOVE -> makeMove(command.getMove());
+            case CONNECT -> connect(command.getUsername(), session, game);
+            case MAKE_MOVE -> makeMove(((MakeMoveCommand) command).getMove());
             case LEAVE -> leave();
             case RESIGN -> resign();
         }
@@ -31,9 +46,10 @@ public class WebsocketHandler {
 
     private void connect(String username, Session session, GameData game) throws IOException {
         connections.add(username, session);
-        String message = String.format("%s has joined %s.", username, game.gameName()); //replace game name with real one
+        String message = String.format("%s has joined %s.", username, game.gameName());
         var loadGame = new LoadGameMessage(game);
         var notification = new NotificationMessage(message);
+        connections.broadcast(username, loadGame);
         connections.broadcast(username, notification);
     }
 
